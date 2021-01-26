1cc400b18527134bfb2a0c4276f139accc19f283
Raphael <dasads>
2021-01-26 17:12:05 -0300

    Instalando Buefy (Biblioteca para UI)

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
+//import { Table, Input } from 'buefy'
+
 Vue.config.productionTip = false
+
+import '@/styles/start.scss'
 
 new Vue({
   store,
